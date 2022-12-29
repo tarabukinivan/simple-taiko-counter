b74fccf4df4f7bd22756812bdf893b6866158966
Leppalúði <105874219+leppaludi@users.noreply.github.com>
2022-12-29 19:47:55 +0000

    hardcode gas limit

--- dApp/pages/index.tsx
+++ dApp/pages/index.tsx
@@ -1,6 +1,7 @@
 import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Web3Button } from "@web3modal/react";
+import { BigNumber } from "ethers";
 import { useState } from "react";
 import { toast } from "react-toastify";
 import {
@@ -42,6 +43,11 @@ export default function HomePage() {
   const { config } = usePrepareContractWrite({
     address: contractAddress,
     abi,
+    args: [
+      {
+        gasLimit: BigNumber.from(500_000),
+      },
+    ],
     functionName: "incrementCounter",
   });
 
